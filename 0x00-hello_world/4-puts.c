5e4f7af78007c975695221095493c773420e9b3e
Blessed-Hinn <machachebenny@hotmail.com>
2022-06-30 15:13:38 -0700

    Commit files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-int main(void)
+int main (void)/*Entry point of the code*/
 {
 	puts("Programming is like building a multilingual puzzle\n");/*Prints information*/
 	return(0);
