e7174782bd864ee494d19b3ea343fe1fcfcca6c1
Blessed-Hinn <machachebenny@hotmail.com>
2022-06-30 15:26:34 -0700

    Changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>/*Including a header file*/
 int main(void)/*Entry point of the program*/
 {
-	puts("Programming is like building a multilingual puzzle\n");/*Prints information*/	
-       	return (0);/*Returns a value*/
+	puts("Programming is like building a multilingual puzzle");/*Prints information*/	
+	return (0);/*Returns a value*/
 }
