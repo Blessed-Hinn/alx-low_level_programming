222ad3c549603de7a4a24f20b24729649f7c2561
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-01 02:45:45 -0700

    Commitinh 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,7 @@
+#include<stdio.h>/*Including a header file*/
+
+int main(void)/*This is the main function,entry point of the program*/
+{
+	printf("with proper grammar, but the outcome is a piece of art,");/*Printing to the standard output*/
+	return(0);/*Returning 0 to the function*/
+}
