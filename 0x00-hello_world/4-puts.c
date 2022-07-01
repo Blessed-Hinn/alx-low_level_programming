7919e768dbaeebf080f18debcf6f87cffd0d6fc6
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-01 07:16:58 -0700

    commiting files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /* This is the entry point of the program*/
-int main(void)/*This shows the main functio*/
+int main(void)/*Description of the main function*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
