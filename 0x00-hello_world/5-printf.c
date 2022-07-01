42f43a372e448ef947dc126fea29b00283363f94
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-01 07:23:09 -0700

    final commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,5 +3,5 @@
 int main(void)/*This is the main function,entry point of the program*/
 {		/*document*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");/*document*/
-	return (0);/*Document*/
+	return (0);
 }
