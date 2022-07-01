64b4d486f015c3d6f0c9522df8184050baa6053f
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-01 08:01:01 -0700

    commit all files

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-/* This is the entry point of the program*/
-int main(void)//Description of the main function
+// This is the entry point of the program.
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
