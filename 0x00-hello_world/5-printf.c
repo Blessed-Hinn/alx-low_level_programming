2d487c4be79b98e4b8d6e9049dd03c4aed5a33b1
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-01 08:28:34 -0700

    commit all

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include<stdio.h>/*Including a header file*/
 /*document*/
 int main(void)/*This is the main function,entry point of the program*/
-{		
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");/*document*/
 	return (0);
 }
