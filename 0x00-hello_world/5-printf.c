636d9486e4ab4a4ce2a2f789a5c37235b3b5afd1
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-01 02:54:34 -0700

    Commitinh 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,6 @@
 
 int main(void)/*This is the main function,entry point of the program*/
 {
-	printf("with proper grammar, but the outcome is a piece of art,");/*Printing to the standard output*/
-	return(0);/*Returning 0 to the function*/
+	printf(stderr,"with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
