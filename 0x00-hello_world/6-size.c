3af24ddc02cf2d63bd5ef63d597a772f00364d2c
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-01 08:14:14 -0700

    commit all

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of a int: %d byte(s)\n",sizeof(b));
 	printf("Size of a long int: %d byte(s)\n",sizeof(c));
 	printf("Size of a long long int: %d byte(s)\n",sizeof(d));
-	prinf("Size of a float: %d byte(s)\n",sizeof(e));
+	printf("Size of a float: %d byte(s)\n",sizeof(e));
 	return (0);
 }
