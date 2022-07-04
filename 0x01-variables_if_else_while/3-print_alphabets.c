22dfe004f32f55e8c7ec61c94e059612cf4ba12f
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-04 06:48:37 -0700

    commit 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ **/
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	if (ch == 'z')
+	{
+		for (ch = 'A'; ch <= 'Z'; ch++)
+		{
+			putchar(ch);
+		if (ch == 'Z')
+		{
+			putchar('\n')
+		}
+		}
+	}
+	}
+}
