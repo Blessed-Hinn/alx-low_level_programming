7f6ee7a528a1fe1e9a95d418dd33930b8c8b3346
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-04 07:37:26 -0700

    commit 5-print_numbers.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0 (success)
+ **/
+
+int main(void)
+{
+	int num;
+
+	for (num == 0; num <= 9 ; num++)
+	{
+		printf("%d", num);
+		if (num == 9)
+			printf("\n");
+	}
+}
