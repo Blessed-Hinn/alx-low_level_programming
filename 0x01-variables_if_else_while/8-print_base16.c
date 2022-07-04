38dbb974e4adcf3d9a2d45f4e32a4f88286937d6
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-04 08:27:29 -0700

    commit 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,10 +9,10 @@ int main(void)
 	int num;
 	int ch;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		if (num == 9)
+		if (num == '9')
 		{
 			for (ch = 'a'; ch <= 'f'; ch++)
 			{
