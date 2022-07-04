096a8cf75406d1f53b5cc0c7a3e695551674c32b
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-04 07:10:13 -0700

    Commit 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (success)
+ **/
+
+int main(void)
+{
+	int ch;
+	
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+		if (ch == 'e' || ch === 'q')
+			continue
+	}
+	return (0);
+}
