ae8222874451aa0844c9c914c7dbec224ec32f99
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-04 19:25:38 -0700

    commit 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	rem = n % 10;
 	if (rem > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5", n, rem);
+		printf("Last digit of %d is %d and is greater than 5\n", n, rem);
 	}
 	else if (rem == 0)
 	{
-		printf("Last digit of %d is %d and is 0", n, rem);
+		printf("Last digit of %d is %d and is 0\n", n, rem);
 	}
 	else
 	{
-		printf("Last digit of %d id %d and is less than 6 and not 0", n, rem);
+		printf("Last digit of %d id %d and is less than 6 and not 0\n", n, rem);
 	}
 	return (0);
 }
