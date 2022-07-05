6f42fbb20889a9eecee795aae0bcf65e6e69d73e
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-05 12:32:54 -0700

    commit file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	print-alphabet_x10();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,6 @@ void print_alphabet(void)
 	{
 		putchar(n);
 		if (n == 'z')
-			_putchar('\n');
+			putchar('\n');
 	}
 }
