75ec222f61503aa9e372242bc073e9cb4ce0b908
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-06 04:37:59 -0700

    commit to local

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <math.h>
+/*
+ * _abs - finds absolute of a number
+ * @n: number being checked
+ * Return: Returns the absolute value
+ */
+
+int _abs(int n)
+{
+	int abs;
+
+	abs = abs(n);
+	return abs;
+}
