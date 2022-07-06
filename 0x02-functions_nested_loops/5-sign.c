f6f6a3b5c6b317aed6631575ce3a6d4103e55561
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-06 09:22:13 -0700

    commit files

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- * print_sign - checks if a number is poisitive,zero or negative
+ * print_sign - prints the sign of a number.
  * @n: Value being checked
  * Return: 1 is positive, 0 if 0, -1 if negative
  */
