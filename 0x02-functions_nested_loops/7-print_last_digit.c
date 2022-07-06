70b8fe0baa86db6e0e1490d1a003597bcff446c4
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-06 05:03:44 -0700

    comit to local computer

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <math.h>
+#include <stdlib.h>
+/*
+ * print_last_digit - prints the last digit
+ * @n: the numbeer being checked
+ * Return: Return the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int m;
+
+	m = abs(n % 10);
+	return (m);
+}
