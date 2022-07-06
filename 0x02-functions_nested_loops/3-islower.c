e52666072fbbf6ddcaecbca0ddce4e3ecd1af9e5
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-06 03:09:22 -0700

    commit files to local computer

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _islower - checks for lowercase characters
+ */
+
+int _islower(int c)
+{
+	char m;
+
+	if (islower(m) == 0)
+	{
+		return 0;
+	}
+	else
+		return 1;
+}
