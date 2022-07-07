2fcf1a689ec53898fe17d60a03a4627a5e74720b
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-06 20:41:52 -0700

    commit to local comp

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,16 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n == 98)
+	{
+		printf("%d\n", n);
+	}
+	else if (n < 98)
 	{
 		while (n <= 98)
 		{
 			printf("%d, ", n);
 			n++;
-			if (n == 98)
-				printf("%d\n", n);
 		}
 	}
 	else
@@ -23,8 +25,6 @@ void print_to_98(int n)
 		{
 			printf("%d, ", n);
 			n--;
-			if (n == 98)
-				printf("%d\n", n);
 		}
 	}		
 }
