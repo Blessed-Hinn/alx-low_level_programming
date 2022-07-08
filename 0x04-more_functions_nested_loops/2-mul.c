ff856d5d0e551ce55f743ac318e5bdb0e000b128
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-08 10:54:13 -0700

    commit to local comp

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ * @a: number to be multiplied.
+ * @b: number to be multiplied.
+ * Return: Retuen value
+ */
+
+int mul(int a, int b)
+{
+	int prod;
+
+	prod = (a * b);
+
+	return (prod);
+}
