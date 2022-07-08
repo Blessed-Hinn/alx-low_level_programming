a55fb541cb300f053898e873d92cfb946efc9319
Blessed-Hinn <machachebenny@hotmail.com>
2022-07-08 13:22:52 -0700

    commiy to local

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_most_numbers- prints the numbers, from 0 to 9, followed by a new line.
+ * print_most_numbers- prints numbers, from 0 to 9, followed by a new line.
  */
 
 void print_most_numbers(void)
@@ -21,7 +21,6 @@ void print_most_numbers(void)
 		{
 			_putchar(a);
 		}
-
 		if (a == '9')
 			_putchar('\n');
 	}
