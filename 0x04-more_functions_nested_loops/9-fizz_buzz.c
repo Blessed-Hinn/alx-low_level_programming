86caefe9f6625359431ec5ce74f1269573b12bc3
Blessed-Hinn <machachebenny@hotmail.com>
2022-09-15 15:04:05 -0700

    commit again

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 /**
- * main - main program
- * Return: Returns 0
+ * main - Lists all numbers fro 1 to 100 with exceptions
+ * Return: Returns 0 when exit
  */
 
 int main(void)
 {
+	int num;
 
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+
+		if (num == 100)
+		{
+			printf("\n");
+		}
+	}
 }
