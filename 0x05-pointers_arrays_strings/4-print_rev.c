78834db1f5b8d71703cacd64d27f7b188f8eb2e3
Blessed-Hinn <machachebenny@hotmail.com>
2022-09-24 05:41:42 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to be reversed
+ * retunr: no return value
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	while (*s != '\0')
+		s++;
+
+	for (i = str - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
