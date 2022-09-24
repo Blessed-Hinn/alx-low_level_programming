8bba9913c943a6344823c457c5f4df704ff5f007
Blessed-Hinn <machachebenny@hotmail.com>
2022-09-24 09:09:49 -0700

    Commit 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 /**
  * puts2 - prints character in alternate
  * @str : string as a parameter
@@ -7,9 +8,13 @@
 
 void puts2(char *str)
 {
-	for (; *str != '\0'; str += 2)
+	int len, i;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i += 2)
 	{
-		_putchar(*str);
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
