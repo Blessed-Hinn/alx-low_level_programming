889366ec5c1a41b6b87312dd9a42b748dba24146
Blessed-Hinn <machachebenny@hotmail.com>
2022-09-24 04:21:10 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - retirns length of a string
+ * @s: the parameter
+ * Return: return length of string
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
